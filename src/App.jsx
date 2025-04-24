00f4adf95b73ae112da430e035013597fce8dfd4
shiningsudipto <shiningsudipto.com>
2025-04-24 15:11:51 +0600

    layout setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import rightSideImg from "../src/assets/signin-right-img.png";
+import vibeEverLogo from "../src/assets/vibeEver.png";
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="grid lg:grid-cols-2 grid-cols-1 bg-primary py-12 px-24 h-screen">
+      <div className="border py-20 px-24">
+        <p>content</p>
+      </div>
+      <div
+        className="rounded-[25px] bg-cover bg-center"
+        style={{ backgroundImage: `url(${rightSideImg})` }}
+      ></div>
     </div>
   );
 }
