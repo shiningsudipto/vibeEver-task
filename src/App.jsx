3bbf9d12cda54dba99be20f559b02d32e9ebf065
shiningsudipto <shiningsudipto.com>
2025-04-25 13:07:40 +0600

    style updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
   const isPassActive = password.length > 0;
   const isEmailActive = email.length > 0;
   return (
-    <>
-      <div className="flex lg:flex-row flex-col 2xl:gap-24 xl:gap-14 gap-5 bg-white 2xl:py-12 xl:py-8 2xl:px-24 xl:px-14 px-5 py-5 lg:h-screen">
-        <div className="lg:w-[50%] py-8 flex flex-col justify-between">
+    <div className="lg:p-10 md:p-8 p-4 bg-gray-100 lg:h-screen">
+      <div className="flex lg:flex-row flex-col 2xl:gap-24 xl:gap-14 gap-5 bg-white rounded-[25px] 2xl:py-12 xl:py-8 2xl:px-24 xl:px-14 px-5 py-5 h-full">
+        <div className="lg:w-[50%] flex flex-col justify-between">
           {/* logo */}
           <img src={vibeEverLogo} alt="" className="h-8 w-[154px]" />
           <div>
@@ -34,14 +34,14 @@ function App() {
                   name="email"
                   placeholder="Email Address"
                   onChange={(e) => setEmail(e.target.value)}
-                  className={`peer w-full h-[78px] border-2 border-dark-50 rounded-full px-6 pt-2 pb-2 text-xl text-gray-900 placeholder-gray-400 focus:placeholder-opacity-100 placeholder-opacity-0  focus:outline-none focus:border-primary bg-bg focus-visible:border-2 focus-visible:border-primary ${
+                  className={`peer w-full h-[60px] border-2 border-dark-50 rounded-full px-6 pt-2 pb-2 text-xl text-gray-900 placeholder-gray-400 focus:placeholder-opacity-100 placeholder-opacity-0  focus:outline-none focus:border-primary bg-bg focus-visible:border-2 focus-visible:border-primary ${
                     isEmailActive && "border-2 border-primary"
                   }`}
                 />
                 <label
                   htmlFor="email"
                   className="absolute left-6 -top-4 text-xl text-gray-500 bg-white px-3 transition-all duration-200
-        peer-placeholder-shown:top-6 peer-placeholder-shown:text-xl
+        peer-placeholder-shown:top-4 peer-placeholder-shown:text-xl
         peer-focus:-top-4 peer-focus:text-xl peer-focus:text-primary peer-focus:px-3 peer-focus:left-8"
                 >
                   Email Address
@@ -55,36 +55,36 @@ function App() {
                   id="password"
                   name="password"
                   placeholder="Password"
-                  className={`peer w-full h-[78px] border-2 border-dark-50 rounded-full px-6 pt-2 pb-2 text-xl text-gray-900 placeholder-gray-400 focus:placeholder-opacity-100 placeholder-opacity-0  focus:outline-none focus:border-primary bg-white focus-visible:border-2 focus-visible:border-primary ${
+                  className={`peer w-full h-[60px] border-2 border-dark-50 rounded-full px-6 pt-2 pb-2 text-xl text-gray-900 placeholder-gray-400 focus:placeholder-opacity-100 placeholder-opacity-0  focus:outline-none focus:border-primary bg-white focus-visible:border-2 focus-visible:border-primary ${
                     isPassActive && "border-2 border-primary"
                   }`}
                 />
                 <label
                   htmlFor="password"
                   className="absolute left-6 -top-4 text-xl text-gray-500 bg-white px-3 transition-all duration-200
-        peer-placeholder-shown:top-6 peer-placeholder-shown:text-xl
+        peer-placeholder-shown:top-4 peer-placeholder-shown:text-xl
         peer-focus:-top-4 peer-focus:text-xl peer-focus:text-primary peer-focus:px-3 peer-focus:left-8 "
                 >
                   Password
                 </label>
                 <button
                   onClick={() => setVisible(!isVisible)}
-                  className="border border-dark-50 hover:border-primary p-3 rounded-full cursor-pointer absolute top-3 right-2 text-dark-200 hover:text-primary"
+                  className="border border-dark-50 hover:border-primary p-2 rounded-full cursor-pointer absolute top-2 right-2 text-dark-200 hover:text-primary"
                 >
                   {isVisible ? (
-                    <IoMdEye className="size-[30px]" />
+                    <IoMdEye className="size-[26px]" />
                   ) : (
-                    <IoMdEyeOff className="size-[30px]" />
+                    <IoMdEyeOff className="size-[26px]" />
                   )}
                 </button>
               </div>
               <a
                 href="#"
-                className="text-end text-primary-200 hover:text-primary text-lg"
+                className="text-end text-primary-200 hover:text-primary text-lg font-semibold"
               >
                 Forget Password?
               </a>
-              <button className="bg-primary h-[78px] text-white rounded-[50px] text-lg font-semibold">
+              <button className="bg-primary-100 border-2 border-primary hover:bg-primary h-[60px] text-white rounded-[50px] text-lg font-semibold">
                 SIGN IN
               </button>
               <div>
@@ -92,11 +92,11 @@ function App() {
                   or continue with
                 </p>
                 <div className="flex items-center justify-center gap-6">
-                  <button className="border border-dark-50 hover:border-primary hover:text-primary text-dark-200 p-3 rounded-full cursor-pointer">
-                    <FaGoogle className="size-[30px]" />
+                  <button className="border border-dark-50 hover:border-primary hover:text-primary text-dark-200 p-2 rounded-full cursor-pointer">
+                    <FaGoogle className="size-[26px]" />
                   </button>
-                  <button className="border border-dark-50 hover:border-primary hover:text-primary text-dark-200 p-3 rounded-full cursor-pointer">
-                    <FaGithub className="size-[30px]" />
+                  <button className="border border-dark-50 hover:border-primary hover:text-primary text-dark-200 p-2 rounded-full cursor-pointer">
+                    <FaGithub className="size-[26px]" />
                   </button>
                 </div>
               </div>
@@ -104,7 +104,7 @@ function App() {
           </div>
           <p className="text-lg text-dark-100 text-center">
             Don’t Have an Account?{" "}
-            <span className="text-primary-200 hover:text-primary">
+            <span className="text-primary-200 hover:text-primary font-semibold">
               Sign Up Now
             </span>
           </p>
@@ -128,7 +128,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
